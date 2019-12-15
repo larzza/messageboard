64095d4a48f144daf0c3f946aa6a60390dff43ac
Unknown <lars.torner@gmail.com>
2019-12-15 11:33:45 +0100

    Add more unit tests.

--- src/test/java/se/axeto/msgboardpoc/MessageServiceTest.java
+++ src/test/java/se/axeto/msgboardpoc/MessageServiceTest.java
@@ -1,7 +1,9 @@
 package se.axeto.msgboardpoc;
 
 import org.junit.jupiter.api.*;
+import se.axeto.msgboardpoc.exceptions.MessageExistException;
 import se.axeto.msgboardpoc.exceptions.MessageNotFoundException;
+import se.axeto.msgboardpoc.exceptions.UserNotFoundException;
 import se.axeto.msgboardpoc.model.Message;
 import se.axeto.msgboardpoc.model.MessageImpl;
 import se.axeto.msgboardpoc.service.InMemoryMessageServiceImpl;
@@ -15,17 +17,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MessageServiceTest {
 
+    // Users
     private static final String KARL_OVE_KNAUSGÅRD = "Karl-Ove Knausgård";
     private static final String OLGA_TOKARCZUK = "Olga Tokarczuk";
     private static final String DORIS_LESSING = "Doris Lessing";
+
+    // Message ids
     private static final String MY_STRUGGLE_1 = "My struggle 1";
     private static final String MY_STRUGGLE_2 = "My struggle 2";
     private static final String MY_STRUGGLE_3 = "My struggle 3";
     private static final String MY_STRUGGLE_4 = "My struggle 4";
     private static final String MY_STRUGGLE_5 = "My struggle 5";
     private static final String MY_STRUGGLE_6 = "My struggle 6";
+    private static final String MARTHA_QUEST = "Martha Quest";
+
+    // Mesages
     private static final String MY_STRUGGLE_1_MESSAGE =
             "My struggle 1 - the first novel in Karl-Ove Knausgårds tale about his life.";
+    private static final String MARTHA_QUEST_MESSAGE =
+            "Martha Quest is the first novel in the series The Children of Violence.";
+
     private static MessageService messageService;
 
     @BeforeEach
@@ -55,9 +66,7 @@ public class MessageServiceTest {
         testDataAuthorToBooksMap.put(KARL_OVE_KNAUSGÅRD, karlOvesBooks);
 
         Map<String, Message> dorisBooks = new HashMap<>();
-        dorisBooks.put("Martha Quest",
-                new MessageImpl(DORIS_LESSING, "Martha Quest",
-                        "Martha Quest is the first novel in the series The Children of Violence."));
+        dorisBooks.put(MARTHA_QUEST, new MessageImpl(DORIS_LESSING, MARTHA_QUEST, MARTHA_QUEST_MESSAGE));
         dorisBooks.put("A Proper Marriage",
                 new MessageImpl(DORIS_LESSING, "A Proper Marriage",
                         "A Proper Marriage is the second novel in the series The Children of Violence."));
@@ -93,23 +102,84 @@ public class MessageServiceTest {
     }
 
     @Test
-    void createMessage() {
+    void createMessageExpectSuccess() {
 
+        // Create message
+        final String newMessage = "A new message about a book";
+        final String messageId = "aMessageId";
+        assertDoesNotThrow(() -> {
+            this.messageService.createMessage(
+                    new MessageImpl(KARL_OVE_KNAUSGÅRD, messageId, newMessage));
+        });
+
+        // Get the created message
+        String createdMessage =
+            assertDoesNotThrow(() -> {
+                return this.messageService.getMessage(
+                    new MessageImpl(KARL_OVE_KNAUSGÅRD, messageId, "")).getMessage();
+            });
+
+        // Verify the created message
+        Assertions.assertEquals(newMessage, createdMessage);
+
+    }
+
+    @Test
+    void createMessageExpectMessageExist() {
+        // Create message
+        String newMessage = "A new message about a book";
+        assertThrows(MessageExistException.class, () -> {
+            this.messageService.createMessage(
+                    new MessageImpl(KARL_OVE_KNAUSGÅRD, MY_STRUGGLE_1, newMessage));
+        });
+    }
+
+    @Test
+    void getMessage() {
+        // Get a message
+        String message =
+            assertDoesNotThrow(() -> {
+                return this.messageService.getMessage(
+                        new MessageImpl(DORIS_LESSING, MARTHA_QUEST, "")).getMessage();
+            });
+
+        // Verify the created message
+        Assertions.assertEquals(MARTHA_QUEST_MESSAGE, message);
+    }
+
+    @Test
+    void getMessageUserNotFound() {
+        // Get message for non existing user
+        assertThrows(UserNotFoundException.class, () -> {
+            this.messageService.getMessage(
+                    new MessageImpl("Dlorisz", MARTHA_QUEST, ""));
+        });
+    }
+
+    @Test
+    void getMessageMessageNotFound() {
+        // User exist, message does not
+        assertThrows(MessageNotFoundException.class, () -> {
+            this.messageService.getMessage(
+                    new MessageImpl(DORIS_LESSING, "MarthaKnausgard", ""));
+        });
     }
 
     @Test
     void updateMessage() {
         // Get message from service
         String myStruggle1Msg =
-                assertDoesNotThrow(() ->
-                { return this.messageService.getMessage(
-                            new MessageImpl(KARL_OVE_KNAUSGÅRD, MY_STRUGGLE_1, "")).getMessage();});
+                assertDoesNotThrow(() -> {
+                    return this.messageService.getMessage(
+                            new MessageImpl(KARL_OVE_KNAUSGÅRD, MY_STRUGGLE_1, "")).getMessage();
+                });
 
         // Update message
         String newMessage = "A new message about a book";
-        assertDoesNotThrow(() ->
-        { this.messageService.updateMessage(
-                new MessageImpl(KARL_OVE_KNAUSGÅRD, MY_STRUGGLE_1, newMessage));});
+        assertDoesNotThrow(() -> {
+            this.messageService.updateMessage(
+                new MessageImpl(KARL_OVE_KNAUSGÅRD, MY_STRUGGLE_1, newMessage));
+        });
 
         // Get the updated message
         String updatedMessage =
@@ -118,6 +188,16 @@ public class MessageServiceTest {
 
         // Verify the updated message
         Assertions.assertEquals(newMessage, updatedMessage);
+    }
+
+    @Test
+    void updateNonExistingMessageExpectMessageNotFound() {
+        // Try to update a non existing message
+        String newMessage = "A new message about a book";
+        assertThrows(MessageNotFoundException.class, () -> {
+            this.messageService.updateMessage(
+                new MessageImpl(KARL_OVE_KNAUSGÅRD, "InvalidMessageId", newMessage));
+        });
     }
 
     @Test
@@ -143,24 +223,4 @@ public class MessageServiceTest {
 
     }
 
-    @Test
-    void getAllUsers() {
-    }
-
-    @Test
-    void getData() {
-    }
-
-    @Test
-    void getUserMessages() {
-
-    }
-
-    @Test
-    void getMessage() {
-    }
-
-    @Test
-    void getUserMessageIds() {
-    }
 }
